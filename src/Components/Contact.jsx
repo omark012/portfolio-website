cc9f869fee18ffff9e38429e6abd5332140ee50e
omark012 <omarasif75@gmail.com>
2024-07-13 15:27:22 +0300

    updated app

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,80 +1,82 @@
-import React, { useState } from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
+import React, { useState } from "react";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Contact = () => {
+  const defaultContact = {
+    name: "",
+    email: "",
+    message: "",
+  };
 
-    const defaultContact = {
-        name: "",
-        email: "",
-        message: "",
-    }
+  const [contact, setContact] = useState(defaultContact);
 
-    const [contact, setContact] = useState(defaultContact);
+  const handleInput = (event) => {
+    const { name, value } = event.target;
 
-    const handleInput = (event) => {
-        const { name, value } = event.target;
+    setContact((prevContact) => ({
+      ...prevContact,
+      [name]: value,
+    }));
+  };
 
-        setContact((prevContact) => ({
-            ...prevContact,
-            [name]: value,
-        }))
-    }
+  const formSubmit = (event) => {
+    event.preventDefault();
+    alert(`Thank you for getting in touch!`);
+    setContact(defaultContact);
+  };
 
-    const formSubmit = (event) => {
-        event.preventDefault();
-        alert(`Thank you for getting in touch!`);
-        setContact(defaultContact);
-    }
+  return (
+    <ScrollAnimation
+      animateIn="fadeIn"
+      animateOut="fadeOut"
+      animateOnce={true}
+      delay={200}
+      offset={225}
+    >
+      <section className="contact" id="contact">
+        <div className="container">
+          <h1 className="contact-heading heading">Contact Me</h1>
+          <div className="contact-container">
+            <p className="contact-alert">
+              Submit the form below to get in touch with me
+            </p>
+            <form className="contact-form" onSubmit={formSubmit}>
+              <div className="input-container flex">
+                <input
+                  required
+                  type="text"
+                  placeholder="Your Name"
+                  value={contact.name}
+                  onChange={handleInput}
+                  name="name"
+                />
+                <input
+                  required
+                  type="email"
+                  placeholder="Your Email"
+                  value={contact.email}
+                  onChange={handleInput}
+                  name="email"
+                />
+              </div>
 
-    return (
-        <ScrollAnimation
-            animateIn="fadeIn"
-            animateOut="fadeOut"
-            animateOnce={true}
-            delay={200}
-            offset={225}
-        >
-            <section className="contact" id="contact">
-                <div className="container">
-                    <h1 className="contact-heading heading">Contact Me</h1>
-                    <div className="contact-container">
-                        <p className="contact-alert">Submit the form below to get in touch with me</p>
-                        <form className='contact-form' onSubmit={formSubmit}>
-                            <div className="input-container flex">
-                                <input
-                                    required
-                                    type="text"
-                                    placeholder="Your Name"
-                                    value={contact.name}
-                                    onChange={handleInput}
-                                    name="name"
-                                />
-                                <input
-                                    required
-                                    type="email"
-                                    placeholder="Your Email"
-                                    value={contact.email}
-                                    onChange={handleInput}
-                                    name="email"
-                                />
-                            </div>
+              <textarea
+                className="message"
+                rows="10"
+                placeholder="Message"
+                value={contact.message}
+                onChange={handleInput}
+                name="message"
+              ></textarea>
+              <button type="submit" className="btn form-btn">
+                Let's Talk
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </ScrollAnimation>
+  );
+};
 
-                            <textarea
-                                className="message"
-                                rows="10"
-                                placeholder="Message"
-                                value={contact.message}
-                                onChange={handleInput}
-                                name="message"
-                            >
-                            </textarea>
-                            <button type="submit" className="btn form-btn">Let's Talk</button>
-                        </form>
-                    </div>
-                </div>
-            </section>
-        </ScrollAnimation>
-    )
-}
-
-export default Contact
+export default Contact;
--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import { useTypewriter, Cursor } from 'react-simple-typewriter';
-import profile from '../images/profile.png';
-import resume from '../data/resume.pdf';
-
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import profile from "../images/profile.png";
+import resume from "../data/resume.pdf";
 
 const Showcase = () => {
-
   const [text] = useTypewriter({
-    words: ['Frontend Developer', 'Traveler'],
+    words: ["Frontend Developer", "Traveler"],
     loop: 0,
-  })
+  });
 
   return (
     <section className="showcase container">
       <div className="showcase-container grid">
-
         <div className="showcase-text">
-          <p className="intro">Hello, I am <span className="name">Omar Khan</span></p>
-          <div className="post">{text}<Cursor cursorColor='red' /></div>
-          <div className="description">I'm a front-end developer located in India. I am a skilled and passionate web designer with experience in creating visually appealing and user friendly website.</div>
+          <p className="intro">
+            Hello, I am <span className="name">Omar Khan</span>
+          </p>
+          <div className="post">
+            {text}
+            <Cursor cursorColor="red" />
+          </div>
+          <div className="description">
+            I am a dedicated front-end developer based in India, specializing in
+            crafting visually captivating and user-friendly websites.
+          </div>
 
           <a href={resume} download="Resume">
-            <button className="btn btn-resume"><span>Resume</span></button>
+            <button className="btn btn-resume">
+              <span>Resume</span>
+            </button>
           </a>
         </div>
         <div className="showcase-image">
@@ -29,7 +36,7 @@ const Showcase = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Showcase
+export default Showcase;
